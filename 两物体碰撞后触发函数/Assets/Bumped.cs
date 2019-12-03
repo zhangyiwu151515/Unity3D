7809bb510e2e5e4e258966e7afa1fc1a8042f68b
zhangyiwu151515 <770824885@qq.com>
2019-12-03 11:39:00 +0800

    Unity3D

---
+++ 两物体碰撞后触发函数/Assets/Bumped.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumped : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        print("已撞上");
+        
+    }
+}
