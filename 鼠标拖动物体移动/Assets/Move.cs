1867f8eab710fae17124cc8df151f4537a876f4e
zhangyiwu151515 <770824885@qq.com>
2019-12-03 11:02:17 +0800

    unity3D

---
+++ 鼠标拖动物体移动/Assets/Move.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    private Vector3 _vec3TargetScreenSpace;// 目标物体的屏幕空间坐标
+    private Vector3 _vec3TargetWorldSpace;// 目标物体的世界空间坐标
+    private static Transform _trans;// 目标物体的空间变换组件
+    private Vector3 _vec3MouseScreenSpace;// 鼠标的屏幕空间坐标
+    private Vector3 _vec3Offset;// 偏移
+                                // Use this for initialization
+    void Awake() { _trans = transform; }
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator OnMouseDown()
+    {
+        // 把目标物体的世界空间坐标转换到它自身的屏幕空间坐标 
+
+        _vec3TargetScreenSpace = Camera.main.WorldToScreenPoint(_trans.position);
+
+        // 存储鼠标的屏幕空间坐标（Z值使用目标物体的屏幕空间坐标） 
+
+        _vec3MouseScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _vec3TargetScreenSpace.z);
+
+        // 计算目标物体与鼠标物体在世界空间中的偏移量 
+
+        _vec3Offset = _trans.position - Camera.main.ScreenToWorldPoint(_vec3MouseScreenSpace);
+
+        // 鼠标左键按下 
+
+        while (Input.GetMouseButton(0))
+        {
+
+            // 存储鼠标的屏幕空间坐标（Z值使用目标物体的屏幕空间坐标）
+
+            _vec3MouseScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _vec3TargetScreenSpace.z);
+
+            // 把鼠标的屏幕空间坐标转换到世界空间坐标（Z值使用目标物体的屏幕空间坐标），加上偏移量，以此作为目标物体的世界空间坐标
+
+            _vec3TargetWorldSpace = Camera.main.ScreenToWorldPoint(_vec3MouseScreenSpace) + _vec3Offset;
+
+            // 更新目标物体的世界空间坐标 
+
+            _trans.position = _vec3TargetWorldSpace;
+
+            // 等待固定更新 
+
+            yield return new WaitForFixedUpdate();
+
+        }
+
+
+    }
+    public static void changeToBloodSky()
+    {
+
+        
+
+
+    }
+  
+}
